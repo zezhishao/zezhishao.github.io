a7c97f1dc3a8bf882b7dbe71265d21c23b1b7aec
zezhishao <shaozezhi@ict.ac.cn>
2026-07-07 07:12:29 +0000

    Deploying to gh-pages from @ zezhishao/zezhishao.github.io@6bb24422b913e19f57e51dd49d2af79d77210381 🚀

--- zh/assets/js/search-data.js
+++ zh/assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-关于",
+    title: "关于",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
